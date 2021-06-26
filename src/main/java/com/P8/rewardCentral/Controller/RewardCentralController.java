0ebf5ba8cb2e533a25812835e7cff1fc0311bf56
Hossam-C <chenaoui.hossam@gmail.com>
2021-06-26 16:23:54 +0200

    ajout logs

--- src/main/java/com/P8/rewardCentral/Controller/RewardCentralController.java
+++ src/main/java/com/P8/rewardCentral/Controller/RewardCentralController.java
@@ -3,6 +3,8 @@ package com.P8.rewardCentral.Controller;
 import com.P8.rewardCentral.DTO.RewardPointsDTO;
 import com.P8.rewardCentral.Exceptions.UserIdException;
 import com.P8.rewardCentral.Service.RewardCentralService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,17 +15,26 @@ import java.util.UUID;
 @RestController
 public class RewardCentralController {
 
+    private Logger logger = LoggerFactory.getLogger(RewardCentralController.class);
+
     @Autowired
     RewardCentralService rewardCentralService;
 
     @GetMapping("/")
     public String rewardCentral() {
+
+        logger.info("Controller : rewardCentral Home");
+
         return "P8 RewardCentral Home";
     }
 
     @GetMapping("/attractionrewardpoints")
     public RewardPointsDTO getAttractionRewardPoints(@RequestParam String attractionId, @RequestParam String userId) throws UserIdException {
+
+        logger.info("Controller : attractionrewardpoints");
+
         if (attractionId.isEmpty() || userId.isEmpty()) {
+            logger.error("Controller : attractionrewardpoints => attractionId is mandatory");
             throw new UserIdException("The parameters attractionId  and userId are mandatory");
         }
         Integer points = rewardCentralService.getAttractionRewardPoints(UUID.fromString(attractionId), UUID.fromString(userId));
--- src/main/java/com/P8/rewardCentral/Service/RewardCentralServiceImpl.java
+++ src/main/java/com/P8/rewardCentral/Service/RewardCentralServiceImpl.java
@@ -1,6 +1,8 @@
 package com.P8.rewardCentral.Service;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import rewardCentral.RewardCentral;
 
@@ -10,14 +12,20 @@ import java.util.UUID;
 @Service
 public class RewardCentralServiceImpl implements RewardCentralService{
 
+    private Logger logger = LoggerFactory.getLogger(RewardCentralServiceImpl.class);
+
     private final RewardCentral rewardCentral;
 
     public RewardCentralServiceImpl() {
+
         Locale.setDefault(Locale.US);
         this.rewardCentral = new RewardCentral();
     }
 
     public int getAttractionRewardPoints(UUID attractionId, UUID userId) {
+
+        logger.debug("getAttractionRewardPoints");
+
         return rewardCentral.getAttractionRewardPoints(attractionId, userId);
     }
 }
